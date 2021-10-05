51892737e8a1d0d420feac5dd572b2147780982d
Will B <tryarc@comcast.net>
2021-10-05 18:29:49 -0400

    Working on prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
 // Assignment code here
 
+//Objects to construct the password
+var allChar = {
+  specialCharacters: "!@#$%^&*()",
+  upperCase: "QWERTYUIOPLKJHGFDSAZXCVBNM",
+  lowerCase: "qwertyuiopasdfghjklzxcvbnm",
+  numbers: "1234567890"
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Collecting user parameters
+function generatePassword() {
+  var passwordLength = parseInt(window.prompt("Between 8 and 128 characters, how many characters would you like your password to contain?"))
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) === true) {
+    alert ("Your password length is invalid, please pick a password between 8 and 128 characters.")
+    generatePassword();
+  }
+  // if (typeOf (passwordLength) = "string"){
+  //   console.log("Hello")
+  // }
+  console.log(passwordLength)
+}
 
 // Write password to the #password input
 function writePassword() {
