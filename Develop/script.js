62608d0ba942d074dce99a8fb495d879fc510e49
Will B <tryarc@comcast.net>
2021-10-12 18:13:42 -0400

    added option prompts

--- Develop/script.js
+++ Develop/script.js
@@ -15,13 +15,35 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var passwordLength = parseInt(window.prompt("Between 8 and 128 characters, how many characters would you like your password to contain?"))
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) === true) {
-    alert ("Your password length is invalid, please pick a password between 8 and 128 characters.")
+    alert("Your password length is invalid, please pick a password between 8 and 128 characters.")
     generatePassword();
   }
-  // if (typeOf (passwordLength) = "string"){
-  //   console.log("Hello")
-  // }
-  console.log(passwordLength)
+  var specialIn = window.confirm ("Would you like special characters");
+  var upperIn = window.confirm ("Would you like Uppercase Letters?");
+  var lowerIn = window.confirm ("Would you like lower case letters");
+  var numbersIn = window.confirm ("Would you like numbers?");
+
+  var passwordOptions = {
+    length: passwordLength,
+    special: specialIn,
+    upper: upperIn,
+    lower: lowerIn,
+    numbers: numbersIn
+  }
+  var password=""
+  if (passwordOptions.special === true){
+    password+= allChar.specialCharacters
+  }
+  if (passwordOptions.upper === true){
+    password+= allChar.upperCase
+  }
+  if (passwordOptions.lower === true){
+    password+= allChar.lowerCase
+  }
+  if (passwordOptions.numbers === true){
+    password+= allChar.numbers
+  }
+  return password;
 }
 
 // Write password to the #password input
